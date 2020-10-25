c5045521805518bcc1649726ce941a5ea1cde06e
Andrei <aikey@mail.ru>
2020-10-25 19:28:33 +0300

    feat: +9 -11 tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = null;
+  for (let i = 0; i < bracketsConfig.length; i++) {
+      brackets = bracketsConfig[i].join('');
+      if (str.includes(brackets)) {
+        str = str.replace(brackets, '')
+        return !Boolean(str)
+      }
+  }
 }
